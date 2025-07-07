ad784d99f5ba54587cdabcd37f0f0e9419011962
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-07-08 01:40:48 +0900

    feat:develop apollo client connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,36 @@ import TrendProperties from "@/libs/homepage/TrendProperties";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
 import useDeviceDetect from '../libs/hooks/useDeviceDetect';
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
 
 
 
 
 const Home: NextPage = () => {
     const device = useDeviceDetect();
+     
 
+    const {
+      loading: getPropertiesLoading,
+      data: getPropertiesData,
+      error: getPropertiesError,
+      refetch: getPropertiesRefetch,
+    } =useQuery(GET_PROPERTIES, {
+      fetchPolicy: "network-only",
+      variables: { 
+     input:{
+        page:1,
+        limit:5,
+        sort:"propertyRank",
+        direction: "DESC",
+        search:{
+        },
+    },
+      },
+    });
+        console.log( "getPropertiesData => ", getPropertiesData);
+      
     if(device == "mobile"){
       return <Stack>HOMEPAGE MOBILE</Stack>
     }else{
